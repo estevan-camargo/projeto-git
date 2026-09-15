3344bbd1f07b085699883608f2cf3bdf3b9ba17f
estevan-camargo <mariestevancamargo@gmail.com>
2026-09-15 10:53:19 -0300

    Adiciona os integrantes da equipe

--- index.php
+++ index.php
@@ -7,5 +7,11 @@
 <body>
  <h1>Projeto da dupla</h1>
  <p>Projeto criado para praticar Git e GitHub.</p>
+  <h2>Integrantes</h2>
+ <ul>
+ <li>Estevan Camargo Becker</li>
+ <li>Felipe Bergamaschi Coelho</li>
+ </ul>
+
 </body>
 </html>
